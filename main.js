7b0a77b54451bbe088ac7a728a4833e0a8285663
Quyền Trần Văn <tranvanquyenx4@gmail.com>
2024-08-01 23:01:00 +0700

    update

--- main.js
+++ main.js
@@ -39,3 +39,29 @@ let autoSwitch = setInterval(() => {
 }, 3000);
 
 
+// Date time colum right 
+function displayDateTime() {
+    const now = new Date();
+    const dayNumber = (now.getDay() === 0) ? 8 : now.getDay() + 1; // Chủ nhật sẽ là 8, các ngày khác cộng thêm 1
+    const options = { 
+        year: 'numeric', 
+        month: 'numeric', 
+        day: 'numeric',
+        hour: '2-digit', 
+        minute: '2-digit', 
+        second: '2-digit', 
+        hour12: true
+    };
+    const dateStr = now.toLocaleDateString('vi-VN', options);
+    const timeStr = now.toLocaleTimeString('vi-VN', {hour12: true});
+
+    document.getElementById('dateTime').innerHTML = `Thứ ${dayNumber}, ${dateStr}, ${timeStr} PM`;
+}
+
+// Gọi hàm lần đầu để hiển thị ngay lập tức
+displayDateTime();
+
+// Cập nhật thời gian mỗi giây
+setInterval(displayDateTime, 1000);
+
+
