77c0a768b84c74f2423cd24ffb0b176f06aacf15
Quyền Trần Văn <tranvanquyenx4@gmail.com>
2024-08-01 11:12:43 +0700

    update view slide

--- main.js
+++ main.js
@@ -39,13 +39,3 @@ let autoSwitch = setInterval(() => {
 }, 2000);
 
 
-// Next Image
-
-$('.carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
-  });
